10e0835394ed6fd1ee8384c19f0b04ec3c605ffb
Anand <umeshanand324@gmail.com>
2026-05-13 23:23:21 +0530

    models

--- so-clothing/backend/models/user.js
+++ so-clothing/backend/models/user.js
@@ -1,41 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true,
-//     },
-
-//     password: {
-//       type: String,
-//       required: true,
-//     },
-
-//     // ADMIN ACCESS
-//     isAdmin: {
-//       type: Boolean,
-//       default: false,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
-// // FIX OVERWRITE MODEL ERROR
-// module.exports =
-//   mongoose.models.User ||
-//   mongoose.model("User", userSchema);
 
 
 const mongoose = require("mongoose");

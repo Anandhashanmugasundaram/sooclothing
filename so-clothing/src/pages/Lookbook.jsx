82aad47632d16790779be749c4217d20364dfcb5
Anand <umeshanand324@gmail.com>
2026-05-19 23:49:44 +0530

    lookbook

--- so-clothing/src/pages/Lookbook.jsx
+++ so-clothing/src/pages/Lookbook.jsx
@@ -148,7 +148,7 @@ export default function Lookbook() {
                   transition
                 "
               >
-                {size.toUpperCase()}
+                {size.size.toUpperCase()}
               </span>
             ))}
 

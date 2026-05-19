e8c6cb9d59b38cfa141e4df1bb82eda32ed19023
Anand <umeshanand324@gmail.com>
2026-05-19 21:40:14 +0530

    scrollup

--- so-clothing/src/components/site/ScrollToTop.jsx
+++ so-clothing/src/components/site/ScrollToTop.jsx
@@ -5,7 +5,13 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "instant",
+      });
+    }, 50);
   }, [pathname]);
 
   return null;
